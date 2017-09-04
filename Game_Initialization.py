32cc402a6338d7b2e4e7fc0632a7e49612e2ce17
royredman4 <royredman4@gmail.com>
2017-09-04 15:44:37 -0700

    Rearranged the Chess Board layout
    -Commented sections of the file as needed
    -Created Board_Colors matrix for easy management of the board
    -Redesigned the chess piece indexing logic and removed methods that were unnecessary from that
    -Implemented the Chess Piece class to add more functionality to it (Delete, move, and Add Piece)
    -Removed Show_Piece, as it wasn't necessary anymore

--- Game_Initialization.py
+++ Game_Initialization.py
@@ -21,10 +21,27 @@ Moves_dictionary = {"Queen":[8,8,8,8,8,8,8,8],
                   "Rook":[8,8,8,8,0,0,0,0],
                   "Bishop":[0,0,0,0,8,8,8,8],
                   "Knight":[2,2,2,2,0,0,0,0],
-                  "Pawn":[1,0,1,0,0,0,0,0]}
+                  "Pawn":[1,0,1,0,1,1,1,1]}
 
+# The matrix that manages the Chess piece objects on the board
 Chess_Matrix = [[None for x in range(8)] for y in range(8)]
 
+# The matrix that manages the chessboard color objects for each piece
+Board_Colors = [[None for x in range(8)] for y in range(8)]
+
+# Manages each square of the chessboard based on its background color
+class Board_Settings():
+    def __init__(self, canvas, def_color, rectangle):
+        self.canvas = canvas
+        self.default_color = def_color
+        self.rectangle_object = rectangle
+    def Set_Color(self, color):
+        self.canvas.itemconfig(self.rectangle_object, fill = color)
+
+    def Reset_Color(self):
+        self.canvas.itemconfig(self.rectangle_object, fill = self.default_color)
+        
+# The master chess class that overwatches all chess pieces
 class Chess_Set(Frame):
     def __init__(self, canvas):
         Frame.__init__(self)
@@ -32,12 +49,8 @@ class Chess_Set(Frame):
         self.White_Pieces = Chess_Managers("White", self.canvas)
         self.Black_Pieces = Chess_Managers("Black", self.canvas)
         Print_Matrix()
-	
-        #self.canvas = canvas
-        #print("White is \n%s" %self.White_Pieces.Chess_items)
-        #print("Black is \n%s" %self.Black_Pieces.Chess_items)
         
-        
+# Manages every chess piece for its group ("White"/"Black" chess pieces) 
 class Chess_Managers():
     def __init__(self, color, canvas):
         self.Chess_color = color
@@ -46,11 +59,11 @@ class Chess_Managers():
         
     def Create_Pieces(self, canvas):
         if (self.Chess_color is "White"):
-            y_coords = "1"
-            next_row = "2"
+            y_coords =  0
+            next_row =  1
         else:
-            y_coords = "8"
-            next_row = "7"
+            y_coords =  7
+            next_row =  6
 
         x_index = 0
         for key in ["Rook", "Knight", "Bishop", "Queen", "King", "Pawn"]:
@@ -61,38 +74,38 @@ class Chess_Managers():
 
             #Creates every chess piece
             for i in range(0, Piece_count[key], 2):
-                x_coords = chr(65 + x_index)
-                current_index = x_coords + y_coords
+                x_coords = x_index
+                current_index = [x_coords, y_coords]
                 print("current_index is %s" % current_index)
-                self.Chess_items.append(Chess_Piece(key, self.Chess_color, current_index))
+                self.Chess_items.append(Chess_Piece(key, self.Chess_color, current_index, canvas))
                 Update_ChessMatrix(self.Chess_items[-1])
-                Show_Piece(canvas, self.Chess_items[-1])
 
                 '''
                 If the same chess piece is also on the other side of the same row,
                 then add it on the other side.
                 '''
                 if (key not in ["King", "Queen"]):
-                    x_coords = chr(72-x_index)
-                    current_index = x_coords + y_coords
+                    x_coords = 7 - x_index
+                    current_index = [x_coords, y_coords]
                     print("Next coordinates are %s" % (current_index))
-                    self.Chess_items.append(Chess_Piece(key, self.Chess_color, current_index))
+                    self.Chess_items.append(Chess_Piece(key, self.Chess_color, current_index, canvas))
                     Update_ChessMatrix(self.Chess_items[-1])
-                    Show_Piece(canvas, self.Chess_items[-1])
                     
                 x_index += 1
     
-
+# Manages a single chess pieces attributes
 class Chess_Piece():
-    def __init__(self, name, color,coordinates):
+    def __init__(self, name, color, coordinates, canvas):
         self.Piece_name = name
         self.Attack_Moves = Moves_dictionary[name]
         self.color = color
         self.x_coords = coordinates[0]
         self.y_coords = coordinates[1]
-        img_location = os.getcwd() + "/Chess_Pieces/%s_Pieces/%s_%s.GIF" %(color, color, name)
-        self.image = PhotoImage(file=img_location)
-
+        self.canvas = canvas
+        self.image = None
+        self.img_Obj = None
+        self.Create_Piece()
+            
         self.hop = False
         self.Diagonal = False
         
@@ -101,32 +114,61 @@ class Chess_Piece():
 
         elif (name is "Pawn"):
             self.diagonal = True
- 
 
+    def Create_Piece(self):
+        x = self.x_coords
+        y = self.y_coords
+        img_location = os.getcwd() + "/Chess_Pieces/%s_Pieces/%s_%s.GIF" %(self.color, self.color, self.Piece_name)
+        self.image = PhotoImage(file=img_location)
+        self.img_Obj = self.canvas.create_image((30+(x *60)), 450-(y * 60), image = self.image)
+
+    def Move_Piece(self, new_x, new_y):
+        x = (new_x - self.x_coords) * 60
+        y = (self.y_coords - new_y) * 60
+        print("Moving x by %d and y by %d" % (x, y))
+        self.canvas.move(self.img_Obj, x, y)
+
+        self.x_coords = new_x
+        self.y_coords = new_y
+
+    # When this piece is destroyed in the game, then remove its image object
+    def Remove_Piece(self):
+        print("Deleting Piece Image")
+        self.canvas.delete(self.img_Obj)
+
+# Creates the whole ChessBoard from scratch
 def Draw_ChessBoard(canvas):
-    f = ["white", "dark gray"]
+    f = ["dark gray", "white"]
     y = 0
     p = False
     
     for z in range(0, 8):
         x = 0
         for q in range(0, 8):
-            canvas.create_rectangle(x, y, x+60, y+60, fill=f[p])
+            temp = canvas.create_rectangle(x, 480-y, x+60, 480-(y+60), fill=f[p])
+            Board_Colors[q][z] = Board_Settings(canvas, f[p], temp)
+
+            print("x:%d y:%d is %s" %(q, z, f[p]))
             p = not p
             x += 60
         f.reverse()
         y += 60
 
+# Updates the matrix, to reflect a chess piece moving
 def Update_ChessMatrix(chess_piece, old_coords= None):
-    new_x = ord(chess_piece.x_coords) - 65
-    new_y = ord(chess_piece.y_coords) - 49
+    new_x = chess_piece.x_coords
+    new_y = chess_piece.y_coords
     print("Moving chess piece to %s x %s" %(new_x, new_y))
     Chess_Matrix[new_x][new_y] = chess_piece
     if old_coords:
-        old_x = ord(old_coords[0]) - 65
-        old_y = ord(old_coords[1]) - 49
+        old_x = old_coords[0]
+        old_y = old_coords[1]
         Chess_Matrix[old_x][old_y] = None
+        print("Old Coordinates were %s and the new coordinates are %s" % ([old_x, old_y], [new_x, new_y]))
 
+        (Board_Colors[old_x][old_y]).Reset_Color()
+
+# Outputs all the chess pieces neatly onto the console (for debugging purposes)
 def Print_Matrix():
     for y in range(8):
         for x in range(8):
@@ -137,23 +179,9 @@ def Print_Matrix():
                     piece_initial = (Chess_Matrix[x][y]).Piece_name[0] + "  "
                 print (piece_initial),
             else:
-                print ("n  "),
+                print ("   "),
         print (" ")
 
+# Returns either the chess piece at the coordinates, or None if nothing is there
 def Get_Piece(x_coord, y_coord):
-    x_letters = {0:"A", 1:"B", 2:"C",3:"D", 4:"E", 5:"F", 6:"G", 7:"H"}
-    x_coord = x_coord/60
-    y_coord = abs((y_coord/60)-7) +1
-    print("You are at %s%s" % (x_letters[x_coord], str(y_coord)))
-    return Chess_Matrix[x_coord][y_coord-1]
-    
-    
-def Show_Piece(canvas, current_piece):
-    if (current_piece is None):
-        return
-    x = ord(current_piece.x_coords) - 65
-    y = ord(current_piece.y_coords) - 49
-    #print ("X is %s, Y is %s" % (30+(x *60), 450-(y * 60)))
-
-    #print("Image location is %s" %current_piece.image)
-    canvas.create_image((30+(x *60)), 450-(y * 60), image = current_piece.image)
+    return Chess_Matrix[x_coord][y_coord]
