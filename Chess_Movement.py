f08a3f49715ed0c7ac52d43b356592f4e5c042ed
royredman4 <royredman4@gmail.com>
2017-09-07 16:59:05 -0700

    Managed rules for players turns

--- Chess_Movement.py
+++ Chess_Movement.py
@@ -12,12 +12,15 @@ from Game_Initialization import Chess_Matrix, Board_Colors, Moves_dictionary, Ge
 Directions = ["North","East","South","West",
               "NorthEast","SouthEast","SouthWest","NorthWest"]
 
+
 # Manages anything that involves the user clicking on the Chess Board
 class Toggle_Piece(Frame):
     def __init__(self, canvas):
         Frame.__init__(self)
         self.move_options = False
         self.canvas = canvas
+        self.Players_Colors = ["Black", "White"]
+        self.Player_One_Turn = True
         self.Movement_options = {}
 
     '''
@@ -28,9 +31,15 @@ class Toggle_Piece(Frame):
     '''
     def Piece_Clicked(self, x_coords, y_coords):
         current_coords = self.Coordinates_to_index(x_coords, y_coords)
-        print("Current coordinates are %s" %(current_coords))
+        print("Current coordinates are %s" % (current_coords))
         chess_piece = Get_Piece(current_coords[0], current_coords[1])
 
+
+        print ("It is player %s turn" % (self.Players_Colors[self.Player_One_Turn]))
+	
+
+        print("Chess piece is %s and Movement Options are %s" % (chess_piece, self.move_options))
+        
         # Checks if the user clicked on the chess board after the movement options were displayed
         if self.move_options is True:
             self.move_options = False
@@ -40,7 +49,7 @@ class Toggle_Piece(Frame):
             for key in Directions:
                 for i in range(len(self.Movement_options[key])):
                     list = self.Movement_options[key][i]
-                    print("Seeing if %s and %s are the same" %(list, current_coords))
+                    print("Seeing if %s and %s are the same" % (list, current_coords))
 
                     # If the clicked coordinates and the movement coordinates are the same
                     if list == current_coords:
@@ -53,15 +62,24 @@ class Toggle_Piece(Frame):
                             
                         self.piece.Move_Piece(current_coords[0], current_coords[1])
                         Update_ChessMatrix(self.piece, [self.x, self.y])
+                        self.Player_One_Turn = not self.Player_One_Turn
+                        return self.Players_Colors[self.Player_One_Turn]
 
         else:
             # Checks if the user clicked on a chess piece or not
             if chess_piece:
-                self.move_options = True
-                print("%s and is %s" %(chess_piece.Piece_name, chess_piece.color))
-                self.Store_Piece_Information(chess_piece)                
+                # Need to check if the chess piece is a NoneType or not
+                if (self.Players_Colors[self.Player_One_Turn] is not chess_piece.color):
+                    return
+                
+                print("%s and is %s" % (chess_piece.Piece_name, chess_piece.color))
+                self.Store_Piece_Information(chess_piece)
                 self.Movement_Options()
-                print("Movement options are %s" %(self.Movement_options))
+                print("Movement options are %s" % (self.Movement_options))
+
+		# If there movement options, then set as True, otherwise False
+                self.move_options = self.Movement_Check()
+
                 self.Show_Hide_Movement("Yellow")
 
     def Store_Piece_Information(self, piece):
@@ -71,7 +89,6 @@ class Toggle_Piece(Frame):
         self.color = piece.color
         self.piece = piece
         
-    
     # Identifies all the movements the chess piece could make
     def Movement_Options(self):
         # This will go for how many directions there are (8)
@@ -126,11 +143,11 @@ class Toggle_Piece(Frame):
     def Knight_Movements(self, Direction):
         Knight_Coordinates = {"North":[[1,2],[-1,2]], "East":[[2,1],[2,-1]],
                               "South":[[-1,-2],[1,-2]],"West":[[-2,1],[-2,-1]]}
-        possible_moves = []                    
+        possible_moves = []
         for i in range(2):
             x_coords = self.x + Knight_Coordinates[Direction][i][0]
             y_coords = self.y + Knight_Coordinates[Direction][i][1]
-            print("X cord is %d and y cord is %d" %(x_coords, y_coords))
+            print("X cord is %d and y cord is %d" % (x_coords, y_coords))
 
             # Checks if the coordinates are outside the chess board boundaries
             if ((x_coords > -1 and x_coords < 8) and (y_coords > -1 and y_coords < 8)):
@@ -178,18 +195,29 @@ class Toggle_Piece(Frame):
                 if ( (Direction not in ["North", "South"]) and (possible_piece is not None) and (self.color is not possible_piece.color) ):
                     possible_moves.append([x, y])
                     
-                # Checks if anything is blocking its way    
+                # Checks if anything is blocking its way
                 elif ((Direction in ["North", "South"]) and (possible_piece is None)):
                     possible_moves.append([x, y])
             
         return possible_moves
+
+    
+    # Checks if there was any movements the chess piece could move 
+    def Movement_Check(self):
+        for path in Directions:
+            if (self.Movement_options[path]):
+                return True
+
+        return False
+        
+    
     
     # Converts the coordinates the user clicked, to the exact index in the Chess_Matrix
     def Coordinates_to_index(self, x_coords, y_coords):
         x_coords = x_coords/60
-        y_coords = abs((y_coords/60)-7) +1
+        y_coords = abs((y_coords/60)-7) + 1
         
-        return [x_coords,y_coords-1]
+        return [x_coords, y_coords-1]
 
     def Delete_Piece(self, x_coords, y_coords):
         del_piece = Chess_Matrix[x_coords][y_coords]
@@ -198,12 +226,11 @@ class Toggle_Piece(Frame):
         print("Deleted piece is "),
         print(Chess_Matrix[x_coords][y_coords])
 
-
     '''
     Either shows a list of locations a chess piece can move on the board,
     or hides that list from the board
     '''
-    def Show_Hide_Movement(self, color= None):
+    def Show_Hide_Movement(self, color=None):
         if (color is None):
             Hide_options = True
         else:
@@ -214,7 +241,7 @@ class Toggle_Piece(Frame):
             for i in range(len(attack_options)):
                 x = attack_options[i][0]
                 y = attack_options[i][1]
-                print("X is %s and Y is %s" %(x, y))
+                print("X is %s and Y is %s" % (x, y))
                 if (Hide_options):
                     (Board_Colors[x][y]).Reset_Color()
                 else:
